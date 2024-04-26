b410f7a814deb138f92ec4f4d9f79f24bdfac604
Alexis Cacayuran <aacacayuran@up.edu.ph>
2024-04-26 11:06:29 +0800

    Add logic to quick play route, merge correct and incorrect answers to an array

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const app = express();
 const port = 3000;
 const API_URL = "https://opentdb.com/";
 
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -19,7 +36,14 @@ app.get("/quick", async (req, res) => {
         type: "multiple",
       },
     });
-    console.log(response.data);
+
+    const results = response.data.results;
+    for (let i = 0; i < 10; i++) {
+      results[i].answers = [...results[i].incorrect_answers];
+      results[i].answers.push(results[i].correct_answer);
+      shuffle(results[i].answers);
+    }
+    console.log(results);
     res.render("quick.ejs");
   } catch (error) {
     console.log("Error: ", error.message);
